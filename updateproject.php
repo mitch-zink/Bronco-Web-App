b63e251a4b04e28e71795802ec761384d7c05ddc
Mitch Zink <47302583+mitch-zink@users.noreply.github.com>
2020-02-11 17:06:33 -0500

    Started updateproject.php
    work in progress

---
+++ updateproject.php
@@ -0,0 +1,193 @@
+<?php
+// ------------------------------
+// Update statement
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bronco";
+
+// Checks for null values
+if (!isset($cat_id_1)) {
+    $cat_id_1 = filter_input(INPUT_GET, 'cat_id_1', FILTER_VALIDATE_INT);
+}
+
+// Checks for a null value
+if (!isset($cat_id_1)) {
+    $cat_id_1 = filter_input(INPUT_GET, 'cat_id_1', FILTER_VALIDATE_INT);
+}
+
+// Checks for a null value
+if (!isset($cat_id_1)) {
+    $cat_id_1 = filter_input(INPUT_GET, 'cat_id_1', FILTER_VALIDATE_INT);
+}
+
+// Checks for a null value
+if (!isset($cat_id_1)) {
+    $cat_id_1 = filter_input(INPUT_GET, 'cat_id_1', FILTER_VALIDATE_INT);
+}
+
+// Checks for a null value
+if (!isset($cat_id_1)) {
+    $cat_id_1 = filter_input(INPUT_GET, 'cat_id_1', FILTER_VALIDATE_INT);
+}
+
+// Checks for a null value
+if (!isset($cat_id_1)) {
+    $cat_id_1 = filter_input(INPUT_GET, 'cat_id_1', FILTER_VALIDATE_INT);
+}
+
+// Checks for a null value
+if (!isset($cat_id_1)) {
+    $cat_id_1 = filter_input(INPUT_GET, 'cat_id_1', FILTER_VALIDATE_INT);
+}
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "UPDATE projects SET projectname='testing' WHERE projectid=2";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . $conn->error;
+}
+
+$conn->close();
+
+// DB connection
+$dsn = 'mysql:host=localhost;dbname=bronco';
+$username = 'root';
+$password = '';
+try {
+    $db = new PDO($dsn, $username, $password);
+    //  echo '<p> You are connected to the database ! </p>';
+    
+}
+catch(PDOException $e) {
+    $error_message = $e->getMessage();
+    //  echo "<p> Connection error! : $error_message</p>";
+    
+}
+// ------------------------------
+
+// ------------------------------
+// Query for DDL
+$query = $db->query("select * from projects order by projectname asc");
+//
+
+// Checks for a null value
+if (!isset($cat_id_1)) {
+    $cat_id_1 = filter_input(INPUT_GET, 'cat_id_1', FILTER_VALIDATE_INT);
+}
+
+// Runs a query based on the selected projectid from the DDL
+$queryprojects = 'SELECT * FROM projects
+   							WHERE projectid = :cat_id_1
+   							ORDER BY projectid';
+$statement2 = $db->prepare($queryprojects);
+$statement2->bindValue(':cat_id_1', $cat_id_1);
+$statement2->execute();
+$projects = $statement2->fetchAll();
+$statement2->closeCursor();
+// ------------------------------
+?>
+
+<!DOCTYPE html>
+<html>
+   <head>
+      <link rel="stylesheet" type="text/css" href="css.css">
+   </head>
+   <body>
+      <ul>
+         <li style="float:left"><a href="#">Bronco</a>
+         <li><a href="home.php">Home</a></li>
+         <li><a href="homepage.php">Admin Home Page</a></li>
+         <li><a href="aboutus.php">About Us</a></li>
+         <li><a href="purpose.php">Purpose</a></li>
+         <li><a href="faq.php">FAQ</a></li>
+         <li><a href="createUA.php">Create project Account</a></li>
+         <li><a href="login.php">Login</a></li>
+         <li><a href="logout.php">Logout</a></li>
+         <li><a href="parts.php">Parts</a></li>
+         <li><a href="phonebook.php">Phonebook</a></li>
+         <li><a href="projects.php">Projects</a></li>
+         <li><a href="files.php">Files</a></li>
+         <li><a href="WorkCompleted.php">Work Completed</a></li>
+         <li><a href="createnewproject.php">Create New Project</a></li>
+         <li><a href="openproject.php">Open Project</a></li>
+      </ul>
+      <!-- DDL -->
+      <div class="form-style-6">
+         <h1>Select a project to continue</h1>
+         <form action="#" name="DDL" method="get">
+            <select name="cat_id_1">
+               <option></option>
+               <?php
+while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+    echo '<option value="' . $row['projectid'] . '">' . $row['projectname'] . '</option>';
+}
+?>
+            </select>
+            <input type="submit" name="action" value="Submit">
+         </form>
+         </form>
+      </div>
+      <script src="js/scripts.js"></script>
+      <div class="form-style-6">
+         <?php
+foreach ($projects as $project):
+?>
+         <h1><?php
+    echo $project["projectname"];
+?></h1>
+         <?php
+endforeach;
+?>
+         <form>
+            <?php
+foreach ($projects as $project):
+?>
+            <input type="text"  name="projectid" placeholder="projectid" value="Project ID: <?php
+    echo $project["projectid"];
+?>" />
+            <input type="text"   name="projectname" placeholder="projectname" value="Project Name: <?php
+    echo $project["projectname"];
+?>" />
+            <input type="text"   name="Make" placeholder="Make" value="Make: <?php
+    echo $project["make"];
+?>" />
+            <input type="text"   name="Model" placeholder="Model" value="Model: <?php
+    echo $project["model"];
+?>" />
+            <input type="text"   name="trim_pkg" placeholder="trim_pkg" value="Trim Package: <?php
+    echo $project["trim_pkg"];
+?>" />
+            <input type="text"   name="projectdesc" placeholder="projectdesc" value="Project Description: <?php
+    echo $project["projectdesc"];
+?>" />
+            <input type="text"   name="purchdate" placeholder="purchdate" value="Purchase Date: <?php
+    echo $project["purchdate"];
+?>" />
+            <input type="text"   name="purchprice" placeholder="purchprice" value="Purchase Price: <?php
+    echo $project["purchprice"];
+?>" />
+            <input type="text"   name="sellprice" placeholder="sellprice" value="Sell Price: <?php
+    echo $project["sellprice"];
+?>" />
+            <input type="text"   name="selldate" placeholder="selldate" value="Sell Date: <?php
+    echo $project["selldate"];
+?>" />
+            <input type="text"   name="projectcomments" placeholder="projectcomments" value="Project Comments: <?php
+    echo $project["projectcomments"];
+?>" />
+            <?php
+endforeach;
+?>
+         </form>
+      </div>
+   </body>
+</html>
