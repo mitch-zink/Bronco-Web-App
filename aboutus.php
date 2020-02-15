d889430a02e6f429a0a33996e482e61076e46307
Mitch Zink <47302583+mitch-zink@users.noreply.github.com>
2020-02-15 09:50:27 -0500

    Updating nav bar

--- aboutus.php
+++ aboutus.php
@@ -7,21 +7,31 @@
 
 <ul>
   <li style="float:left"><a href="#">Bronco</a>
-  <li><a href="home.php">Home</a></li>
-  <li><a href="homepage.php">Admin Home Page</a></li>
-  <li><a class="active" href="aboutus.php">About Us</a></li>
-  <li><a href="purpose.php">Purpose</a></li>
-  <li><a href="faq.php">FAQ</a></li>
-  <li><a href="createUA.php">Create User Account</a></li>
-  <li><a href="login.php">Login</a></li>
-  <li><a href="logout.php">Logout</a></li>
-  <li><a href="parts.php">Parts</a></li>
-  <li><a href="phonebook.php">Phonebook</a></li>
-  <li><a href="projects.php">Projects</a></li>
-  <li><a href="files.php">Files</a></li>
-  <li><a href="WorkCompleted.php">Work Completed</a></li>
-  <li><a href="createnewproject.php">Create New Project</a></li>
-  <li><a href="openproject.php">Open Project</a></li>
+  <li><a href="aboutus.php">aboutus</a></li>
+  <li><a href="addcontactform.php">addcontactform</a></li>
+  <li><a href="addpartsform.php">addpartsform</a></li>
+  <li><a href="addtransactionform.php">addtransactionform</a></li>
+  <li><a href="createnewproject.php">createnewproject</a></li>
+  <li><a href="createua.php">createua</a></li>
+  <li><a href="dropbox.php">dropbox</a></li>
+  <li><a href="faq.php">faq</a></li>
+  <li><a href="files.php">files</a></li>
+  <li><a href="home.php">home</a></li>
+  <li><a href="homepage.php">homepage</a></li>
+  <li><a href="inventory.php">inventory</a></li>
+  <li><a href="login.php">login</a></li>
+  <li><a href="logout.php">logout</a></li>
+  <li><a href="openedproject.php">openedproject</a></li>
+  <li><a href="openproject.php">openproject</a></li>
+  <li><a href="parts.php">parts</a></li>
+  <li><a href="phonebook.php">phonebook</a></li>
+  <li><a href="projects.php">projects</a></li>
+  <li><a href="purpose.php">purpose</a></li>
+  <li><a href="report2.php">report2</a></li>
+  <li><a href="updateproject.php">updateproject</a></li>
+  <li><a href="upload.php">upload</a></li>
+  <li><a href="viewtransaction.php">viewtransaction</a></li>
+  <li><a href="workcompleted.php">workcompleted</a></li>
 </ul>
 <?php 
 echo "<h3>About Us</h3>";
