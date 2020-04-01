a8c16f915418382230e19f954f2aaf480ad0ea2a
Ryn0113 <40875234+Ryn0113@users.noreply.github.com>
2020-04-01 14:34:34 -0400

    parts quantities should update correctly when transaction is added or updated

--- parts/addTransaction.php
+++ parts/addTransaction.php
@@ -8,34 +8,91 @@ $date = filter_input(INPUT_POST, "date");
 $quantity = filter_input(INPUT_POST, "quantity");
 $partid = filter_input(INPUT_POST, "partid");
 
-/*//validate
-if ($phoneid === null || $type === null || $price === null || 
-		$date === null || $quantity === null || $partid === null) { 
-        
-        $error = "Invalid data. Check all fields and try again.";
+//Select part quantity
+$sql4 = "SELECT quantity FROM parts WHERE partid = :partid";
+$stmt4 = $db->prepare($sql4);
+$stmt4->bindValue(":partid", $partid);
+$stmt4->execute();
+$quant = $stmt4->fetch();
+$stmt4->closeCursor();
+
+//var_dump($quant);
+
+//Compare transaction quantity to part quantity 
+$tquant = 0;
+if($type === "Seller"){
+  $tquant = $quant['quantity'] - $quantity; 
+  
+  //Validate current part inventory
+  if($tquant < 0){
+    $error = "Insufficent quantity in inventory. Check inventory and try again.";
     echo $error;
+    
+    }
+    else {
+    
+  //update parts quantity
+    $sql = 'UPDATE parts SET quantity = :tquant WHERE partid = :partid';
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(':tquant', $tquant);
+    $stmt->bindValue(':partid', $partid);
+    $stmt->execute();
+    $stmt->closeCursor();
+    
+    //Insert Transaction
+	$sql1 = 'INSERT INTO transactions
+    (phoneid, partid, date, price, transtype, quantity)
+  VALUES
+    (:phoneid, :partid, :date, :price, :type, :quantity)';
+
+    $stmt1 = $db->prepare($sql1);
+    $stmt1->bindValue(':phoneid', $phoneid);
+    $stmt1->bindValue(':partid', $partid);
+    $stmt1->bindValue(':date', $date);
+    $stmt1->bindValue(':price', $price);
+    $stmt1->bindValue(':type', $type);
+    $stmt1->bindValue(':quantity', $quantity);
+            
+    $stmt1->execute();
+    $stmt1->closeCursor();
+
+    }
+    include('viewTransaction.php');    
 }
 
-else { */
+if($type === "Buyer"){
+    $tquant = $quantity + $quant['quantity'];
+
+//var_dump($tquant);
+
+    $sql2 = 'UPDATE parts SET quantity = :tquant WHERE partid = :partid';
+    $stmt2 = $db->prepare($sql2);
+    $stmt2->bindValue(':tquant', $tquant);
+    $stmt2->bindValue(':partid', $partid);
+    $stmt2->execute();
+    $stmt2->closeCursor();
    
-    //Insert vendor 
-	$sql = 'INSERT INTO transactions
+    //Insert Transaction
+	  $sql3 = 'INSERT INTO transactions
 				(phoneid, partid, date, price, transtype, quantity)
 			  VALUES
 				(:phoneid, :partid, :date, :price, :type, :quantity)';
       
-      $stmt = $db->prepare($sql);
-      $stmt->bindValue(':phoneid', $phoneid);
-      $stmt->bindValue(':partid', $partid);
-      $stmt->bindValue(':date', $date);
-      $stmt->bindValue(':price', $price);
-      $stmt->bindValue(':type', $type);
-      $stmt->bindValue(':quantity', $quantity);
-                     
-      $stmt->execute();
-      $stmt->closeCursor();
-//}     
-include('viewTransaction.php');
+    $stmt3 = $db->prepare($sql3);
+    $stmt3->bindValue(':phoneid', $phoneid);
+    $stmt3->bindValue(':partid', $partid);
+    $stmt3->bindValue(':date', $date);
+    $stmt3->bindValue(':price', $price);
+    $stmt3->bindValue(':type', $type);
+    $stmt3->bindValue(':quantity', $quantity);
+    $stmt3->execute();
+    $stmt3->closeCursor();
+    
+    include('viewTransaction.php');  
+}   
+
+    
+
 
 
 
--- parts/updateTransaction.php
+++ parts/updateTransaction.php
@@ -10,33 +10,89 @@ $price = filter_input(INPUT_POST, "price");
 $date = filter_input(INPUT_POST, "date");
 $quantity = filter_input(INPUT_POST, "quantity");
 
+//Select part quantity
+$sql4 = "SELECT quantity FROM parts WHERE partid = :partid";
+$stmt4 = $pdo->prepare($sql4);
+$stmt4->bindValue(":partid", $partid);
+$stmt4->execute();
+$quant = $stmt4->fetch();
+$stmt4->closeCursor();
 
-/*//validate
-if ($transid === null || $phoneid === null || $partid === null || $type === null || $price === null || 
-		$date === null || $quantity === null) { 
-        
-        $error = "Invalid data. Check all fields and try again.";
+//var_dump($quant);
+
+//Compare transaction quantity to part quantity 
+$tquant = 0;
+if($type === "Seller"){
+  $tquant = $quant['quantity'] - $quantity; 
+  
+  //var_dump($tquant);
+  //Validate current part inventory
+  if($tquant < 0){
+    $error = "Insufficent quantity in inventory. Check inventory and try again.";
     echo $error;
+    
+    }
+    else {
+    
+  //update parts quantity
+    $sql = 'UPDATE parts SET quantity = :tquant WHERE partid = :partid';
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':tquant', $tquant);
+    $stmt->bindValue(':partid', $partid);
+    $stmt->execute();
+    $stmt->closeCursor();
+    
+    //Update Transaction
+	$sql1 = 'UPDATE transactions
+        SET phoneid =:phoneid, partid =:partid, transtype =:type, price =:price, 
+        date =:date, quantity =:quantity WHERE transid =:transid';
+      
+        $stmt1 = $pdo->prepare($sql1);
+        $stmt1->bindValue(':phoneid', $phoneid);
+        $stmt1->bindValue(':partid', $partid);
+        $stmt1->bindValue(':type', $type);
+        $stmt1->bindValue(':date', $date);
+        $stmt1->bindValue(':price', $price);
+        $stmt1->bindValue(':quantity', $quantity);
+        $stmt1->bindValue(':transid', $transid); 
+        $stmt1->execute();
+        $stmt1->closeCursor();
+
+    }
+    include('viewTransaction.php');    
 }
 
-else { */
+if($type === "Buyer"){
+    $tquant = $quantity + $quant['quantity'];
 
+//var_dump($tquant);
+
+    //update part quantity
+    $sql2 = 'UPDATE parts SET quantity = :tquant WHERE partid = :partid';
+    $stmt2 = $pdo->prepare($sql2);
+    $stmt2->bindValue(':tquant', $tquant);
+    $stmt2->bindValue(':partid', $partid);
+    $stmt2->execute();
+    $stmt2->closeCursor();
    
-    //update vendor 
-	$sql = 'UPDATE transactions
-                SET phoneid =:phoneid, partid =:partid, transtype =:type, price =:price, 
-                date =:date, quantity =:quantity WHERE transid =:transid';
+    //update transaction
+    $sql3 = 'UPDATE transactions
+        SET phoneid =:phoneid, partid =:partid, transtype =:type, price =:price, 
+        date =:date, quantity =:quantity WHERE transid =:transid';
       
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindValue(':phoneid', $phoneid);
-        $stmt->bindValue(':partid', $partid);
-        $stmt->bindValue(':type', $type);
-        $stmt->bindValue(':date', $date);
-        $stmt->bindValue(':price', $price);
-        $stmt->bindValue(':quantity', $quantity);
-        $stmt->bindValue(':transid', $transid); 
-        $stmt->execute();
-        $stmt->closeCursor();
-//}
-    include('viewTransaction.php');
+        $stmt3 = $pdo->prepare($sql3);
+        $stmt3->bindValue(':phoneid', $phoneid);
+        $stmt3->bindValue(':partid', $partid);
+        $stmt3->bindValue(':type', $type);
+        $stmt3->bindValue(':date', $date);
+        $stmt3->bindValue(':price', $price);
+        $stmt3->bindValue(':quantity', $quantity);
+        $stmt3->bindValue(':transid', $transid); 
+        $stmt3->execute();
+        $stmt3->closeCursor();
+    
+    
+
+    include('viewTransaction.php');  
+}  
 ?>
