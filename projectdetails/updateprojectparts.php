a8526385e0ec35e7d3fd35b78ea7e73a6263d5cf
Ryn0113 <40875234+Ryn0113@users.noreply.github.com>
2020-04-21 12:01:03 -0400

    Update parts now let you add and subract while keeping parts inventory counts.

--- projectdetails/updateprojectparts.php
+++ projectdetails/updateprojectparts.php
@@ -26,9 +26,42 @@ $stmt2->closeCursor();
 
 //var_dump($count['quantity']);
 
-$q = 0;
+
 $partq = 0;
+if($pquantity > $ppq['quantity']){
+        $addq = $pquantity - $ppq['quantity'];
+        if($addq > $count['quantity']) {
+                $error = "Error: Invalid inventory. Quantity exceeds inventory quantity on hand.";
+                 echo $error;
+        }
+        else {      
+                $partq =  $count['quantity'] - $addq;
+
+                //update part inventory
+                $sql = 'UPDATE parts
+                SET quantity =:partq WHERE partid =:partid';
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindValue(':partq', $partq);
+                $stmt->bindValue(':partid', $partid);
+                $stmt->execute();
+                $stmt->closeCursor();
+
+
+                //update project parts
+                $sql1 = 'UPDATE project_parts
+                        SET quantity =:pquantity WHERE partid =:partid AND projectid =:projectid';
+                $stmt1 = $pdo->prepare($sql1);
+                $stmt1->bindValue(':pquantity', $pquantity);
+                $stmt1->bindValue(':partid', $partid);
+                $stmt1->bindValue(':projectid', $projectid);
+                $stmt1->execute();
+                $stmt1->closeCursor();  
+        }
+        
+        
+}
 if($pquantity < $ppq['quantity']){
+       
      $dif = $ppq['quantity'] - $pquantity;
      $partq = $dif + $count['quantity'];  
 
@@ -50,41 +83,19 @@ if($pquantity < $ppq['quantity']){
      $stmt1->bindValue(':projectid', $projectid);
      $stmt1->execute();
      $stmt1->closeCursor();    
-
+     
 }
 
-$q = $count['quantity'] - $pquantity;
+if($pquantity < 0){
+        $error = "Error: Quantity must be 0 or higher. Please re-enter quantity";
+        echo $error;
+}
 
-//Compare inventory quantity to project parts request
+/*Compare inventory quantity to project parts request
 if($q < 0){
     $error = "Error: Invalid inventory. Quantity exceeds inventory quantity on hand.";
     echo $error;
-}
-
-else{
-
-//update part inventory
-$sql1 = 'UPDATE parts
-        SET quantity =:q WHERE partid =:partid';
-
-$stmt1 = $pdo->prepare($sql1);
-$stmt1->bindValue(':q', $q);
-$stmt1->bindValue(':partid', $partid);
-$stmt1->execute();
-$stmt1->closeCursor();
-
-//update project parts
-$sql1 = 'UPDATE project_parts
-        SET quantity =:pquantity WHERE partid =:partid AND projectid =:projectid';
-
-$stmt1 = $pdo->prepare($sql1);
-$stmt1->bindValue(':pquantity', $pquantity);
-$stmt1->bindValue(':partid', $partid);
-$stmt1->bindValue(':projectid', $projectid);
-$stmt1->execute();
-$stmt1->closeCursor();    
-
-}
+}*/
 
 include('projectparts.php');
 ?>
